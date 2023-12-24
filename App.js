35992c929258cce78676c12e821c34949a549ef1
Md Nuruddin <nuruddinalways99@gmail.com>
2023-12-24 21:26:40 +0600

    Ring animation using react native

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Feather } from "@expo/vector-icons";
+import { MotiView } from "@motify/components";
+import * as React from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
+const _color = "dodgerblue";
+const _size = 100;
+
+const WaveTingy = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <View style={[styles.dot, styles.center]}>
+        {[...Array(3).keys()].map((index) => {
+          return (
+            <MotiView
+              from={{ opacity: 0.7, scale: 1 }}
+              animate={{ opacity: 0, scale: 5 }}
+              transition={{
+                type: "timing",
+                duration: 2000,
+                delay: index * 400,
+                loop: true,
+              }}
+              key={index}
+              style={[StyleSheet.absoluteFillObject, styles.dot]}
+            />
+          );
+        })}
+
+        <Feather name='phone-call' size={32} color='#fff' />
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  dot: {
+    width: _size,
+    height: _size,
+    borderRadius: _size,
+    backgroundColor: _color,
+  },
+  center: {
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+export default WaveTingy;
